48d528bedeb12442268040eabac3b357162287ce
Joksar <politer72@gmail.com>
2021-08-17 17:21:56 +0300

    Lesson 1 Homework

---
+++ Lesson 1/script.js
@@ -0,0 +1,19 @@
+//Упражнение 1
+
+var temp_C = 30;
+var temp_F = (9 / 5) * temp_C + 32;
+alert("t по цельсию = " + temp_C + ". t по фаренгейту = " + temp_F);
+console.log("t по цельсию = " + temp_C + ". t по фаренгейту = " + temp_F);
+
+
+//Упражнение 2
+
+var admin;
+var name;
+name = "Василий";
+admin = name;
+alert(admin)
+
+/*Упражнение 3
+Выражение будет равно 1000108. Тип данных - строка
+*/
